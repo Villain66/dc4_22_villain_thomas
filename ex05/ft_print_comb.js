bf1f1d763509f8d0909b4734f42d418fab6a1d1e
Villain66 <t.villainpro@gmail.com>
2023-02-19 22:38:21 +0100

    creation combinaison de trois chiffres

---
+++ ex05/ft_print_comb.js
@@ -0,0 +1,10 @@
+function ft_print_comb(){
+    for (let i = 0; i <= 9; i++) {
+        for (let j = i + 1; j <= 9; j++) {
+        for (let k = j + 1; k <= 9; k++) {
+        console.log(`${i}${j}${k}`);
+        }
+        }
+        }
+        }
+ft_print_comb();
